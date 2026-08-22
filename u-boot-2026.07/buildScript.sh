89842df8e4a006dce31f5c61bfc35857a825ae9c
yjrqz777 <3210551161@qq.com>
2026-08-22 12:12:09 +0800

    add build script

---
+++ u-boot-2026.07/buildScript.sh
@@ -0,0 +1,9 @@
+
+
+make O=build mt7621_rfb_defconfig
+make O=build menuconfig
+make O=build -j$(nproc)
+
+cp build/u-boot-mt7621.bin /mnt/d/document/github/hlk/SNANDer/flash/u-boot-mt7621.bin
+
+.\Droid-MAX_SNANDer\src\snander.exe -S 10M -a 0x000000 -l 0x7D000 -e
